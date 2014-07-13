1140f2e4be512a58882a8676b94e4b7bdbd6ccfc
sjbgit <sjb_strat@hotmail.com>
2014-07-12 20:10:08 -0500

    added server and index.jade

--- Multivision/server.js
+++ Multivision/server.js
@@ -6,3 +6,11 @@ var express = require('express');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+//match all routes - all requests
+app.get('*', function(req, res) {
+    res.render('index');
+})
