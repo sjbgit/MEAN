23892f01fb8657bde9710b31411edbcb35a76c41
sjbgit <sjb_strat@hotmail.com>
2014-07-14 00:17:19 -0500

    packages

--- Multivision/server.js
+++ Multivision/server.js
@@ -2,14 +2,22 @@
  * Created by Seth on 7/12/2014.
  */
 var express = require('express'),
-    stylus = require('stylus');
+    stylus = require('stylus'), //add lib
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compiile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
 app.use(stylus.middleware(
     {
         src: __dirname + '/public',
