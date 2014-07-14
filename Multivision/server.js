810220034c0ad43ad642b215d6b246114a19e997
sjbgit <sjb_strat@hotmail.com>
2014-07-13 23:56:18 -0500

    modify jade file

--- Multivision/server.js
+++ Multivision/server.js
@@ -14,3 +14,8 @@ app.set('view engine', 'jade');
 app.get('*', function(req, res) {
     res.render('index');
 })
+
+var port = 3030;
+app.listen(port);
+console.log("listening on port " + port + "...");
+
