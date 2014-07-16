42160ab62bba6dc2d204aab3e19388745208077e
sjbgit <sjb_strat@hotmail.com>
2014-07-16 13:16:44 -0500

    added mongoose

--- Multivision/server.js
+++ Multivision/server.js
@@ -4,7 +4,8 @@
 var express = require('express'),
     stylus = require('stylus'), //add lib
     logger = require('morgan'), //importing function
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -25,6 +26,14 @@ app.use(stylus.middleware(
     }
 ));
 app.use(express.static(__dirname + '/public'));//static route to public dir
+
+mongoose.connect('mongodb://mongoguy:d0m1c1l3@ds029837.mongolab.com:29837/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+   console.log('multivision db opened');
+
+});
 
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
